46124cf2f9d862a31632590815b9784f5f04b870
Faizan Pasha <faizanpasha265@gmail.com>
2025-08-27 19:29:20 +0530

    Initial Commit

---
+++ lib/App/Project/ProjectPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProjectPage extends StatefulWidget {
+  const ProjectPage({Key? key}) : super(key: key);
+
+  @override
+  ProjectPageState createState() => ProjectPageState();
+}
+
+class ProjectPageState extends State<ProjectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('ProjectPage'));
+  }
+}
